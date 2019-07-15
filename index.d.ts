e8b789fbef2ef6129620999e675906f317105d49
Rupert Moore <rupertmoore@ruby-imac.local>
2019-07-15 21:12:56 +1000

    init
    Signed-off-by: Rupert Moore <rupertmoore@ruby-imac.local>

---
+++ index.d.ts
@@ -0,0 +1,33 @@
+declare module 'd3-weighted-voronoi' {
+  function weightedVoronoi<T = any>(): WVLayout<T>;
+  export default weightedVoronoi;
+  type pt = [number, number];
+  export interface WVLayout<T> {
+    (sites: T[]): Array<WVPolygon<T>>;
+    x(fx: (d: T) => number): this;
+    x(): (d: T) => number;
+    y(fy: (d: T) => number): this;
+    y(): (d: T) => number;
+    weight(fw: (d: T) => number): this;
+    weight(): (d: T) => number;
+    clip(pts: pt[]): this;
+    clip(): pt[];
+    extent(ext: [[pt, pt], [pt, pt]]): this;
+    extent(): [[pt, pt], [pt, pt]];
+    size(sz: [pt, pt]): this;
+    size(): [pt, pt];
+  }
+  export interface WVVertex<T> {
+    x: number;
+    y: number;
+    weight: number;
+    neigbours: Array<WVVertex<T>>;
+    originalObject: T;
+    polygon: WVPolygon<T>;
+    nonClippedPolygon: WVPolygon<T>;
+  }
+  export interface WVPolygon<T> {
+    [i: number]: pt;
+    site: WVVertex<T>;
+  }
+}
